8ac1344dc37180492d943e8238ce9ce5196452da
LuisLuna810 <luis.agustin10@hotmail.com>
2023-07-19 10:44:01 -0300

    fix appbar, products

--- Front/src/components/products/SingleProductDesktop.jsx
+++ Front/src/components/products/SingleProductDesktop.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from "react";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import {
   Product,
   ProductActionButton,
   ProductActionsWrapper,
   ProductAddToCart,
   ProductImage,
-
 } from "../../styles/product";
 import { Stack, Tooltip, Typography, Box } from "@mui/material";
 import FitScreenIcon from "@mui/icons-material/FitScreen";
@@ -15,16 +14,13 @@ import ProductDetail from "../productdetail";
 import ProductMeta from "./ProductMeta";
 import ColorSelect from "../selectcolor/selectcolores";
 import PriceSelect from "../selectprice/PriceSelect";
-import { addProduct, removeProduct, updateQuantity } from "../../state/slices/CartSlice";
-
-
-
-
-
-
+import {
+  addProduct,
+  removeProduct,
+  updateQuantity,
+} from "../../state/slices/CartSlice";
 
 export default function SingleProductDesktop({ product, matches }) {
-
   const [selectedColor, setSelectedColor] = useState(null);
   const [selectedPrice, setSelectedPrice] = useState(product?.prices?.[0]);
   const handleColorChange = (event) => {
@@ -35,7 +31,6 @@ export default function SingleProductDesktop({ product, matches }) {
   const handlePriceChange = (event) => {
     setSelectedPrice(event.target.value);
   };
-
 
   useEffect(() => {
     if (product?.Stocks?.length > 0) {
@@ -49,21 +44,19 @@ export default function SingleProductDesktop({ product, matches }) {
     const { id, name, image } = product;
     const quantity = 1; // O la cantidad deseada
 
-    const productData =
-    {
+    const productData = {
       id,
       name: name + (selectedPrice.size ? " " + selectedPrice.size : ""), // si tiene tamaño, se le agrega al nombre
-      image, size: selectedPrice.size,
+      image,
+      size: selectedPrice.size,
       price: selectedPrice.price,
       quantityPrice: selectedPrice.quantity,
       quantity,
       color: selectedColor.CodigoColor,
-      ColorName: selectedColor.ColorName
+      ColorName: selectedColor.ColorName,
     };
     dispatch(addProduct(productData));
-
   };
-
 
   const [ProductDetailDialog, showProductDetailDialog, closeProductDialog] =
     useDialogModal(ProductDetail);
@@ -78,38 +71,76 @@ export default function SingleProductDesktop({ product, matches }) {
   };
   return (
     <>
-      <Product onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={() => showProductDetailDialog()}>
-
-        <Box sx={{ maxWidth: "300px", maxHeight: "300px", minWidth: "300px", minHeight: "300px" }}>
+      <Product
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onClick={() => showProductDetailDialog()}
+      >
+        <Box
+          sx={{
+            maxWidth: "300px",
+            maxHeight: "300px",
+            minWidth: "300px",
+            minHeight: "300px",
+          }}
+        >
           <ProductImage src={product.image} />
         </Box>
         {(showOptions || matches) && (
-          <ProductAddToCart show={showOptions} variant="contained" onClick={handleAddToCart}>
+          <ProductAddToCart
+            show={showOptions}
+            variant="contained"
+            onClick={handleAddToCart}
+          >
             Add to cart
           </ProductAddToCart>
         )}
         <ProductActionsWrapper show={showOptions || matches}>
-          <Stack direction={matches ? "row" : "column"}>
-          </Stack>
+          <Stack direction={matches ? "row" : "column"}></Stack>
         </ProductActionsWrapper>
       </Product>
-      <Box sx={{ position: "relative", display: "flex", justifyContent: "flex-start", marginLeft: "0%", paddingBottom: "38px", width: "100%" }}>
-
+      <Box
+        sx={{
+          position: "relative",
+          display: "flex",
+          justifyContent: "flex-start",
+          marginLeft: "0%",
+          paddingBottom: "38px",
+          width: "100%",
+        }}
+      >
         <Box sx={{ position: "absolute", marginLeft: "10%" }}>
-          <ColorSelect product={product} selectedColor={selectedColor} setSelectedColor={setSelectedColor} handleColorChange={handleColorChange} />
+          <ColorSelect
+            product={product}
+            selectedColor={selectedColor}
+            setSelectedColor={setSelectedColor}
+            handleColorChange={handleColorChange}
+          />
         </Box>
-        {product?.prices?.[0]?.size ? // Si hay tamaño renderiza el selector
+        {product?.prices?.[0]?.size ? ( // Si hay tamaño renderiza el selector
           <Box sx={{ position: "absolute", marginLeft: "55%" }}>
-            <PriceSelect product={product} selectedPrice={selectedPrice} setSelectedPrice={setSelectedPrice} handlePriceChange={handlePriceChange} />
+            <PriceSelect
+              product={product}
+              selectedPrice={selectedPrice}
+              setSelectedPrice={setSelectedPrice}
+              handlePriceChange={handlePriceChange}
+            />
           </Box>
-          :
-          <></>}
-
-
+        ) : (
+          <></>
+        )}
       </Box>
 
       <ProductMeta product={product} selectedPrice={selectedPrice} />
-      <ProductDetailDialog product={product} selectedPrice={selectedPrice} setSelectedPrice={setSelectedPrice} handlePriceChange={handlePriceChange} selectedColor={selectedColor} setSelectedColor={setSelectedColor} handleColorChange={handleColorChange} />
+      <ProductDetailDialog
+        product={product}
+        selectedPrice={selectedPrice}
+        setSelectedPrice={setSelectedPrice}
+        handlePriceChange={handlePriceChange}
+        selectedColor={selectedColor}
+        setSelectedColor={setSelectedColor}
+        handleColorChange={handleColorChange}
+      />
     </>
   );
 }
